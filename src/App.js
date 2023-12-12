3bbc2a3428f5ead340787f11094292de52334c50
Avi Rana <itzavi1718@gmail.com>
2023-12-12 23:45:33 +0530

    Fixed redirect links and added basic 404 page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Blog from './components/Blog';
+
+import NotFound from './components/NotFound';
+
 import './App.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -12,9 +15,24 @@ function App() {
     <Routes>
     <Route path="/" element={<Main/>} />
     <Route path="/blog" element={<Blog/>} />
+    {/* Redirect links */}
+    <Route path="/github" element={<Github />} />
+    <Route path="/linkedin" element={<LinkedIn />} />
+    <Route path="/twitter" element={<Twitter />} />
+    <Route path="*" element={<NotFound />} />
     </Routes>
     </BrowserRouter>
   );
+}
+
+function Github() {
+  window.location.replace("https://github.com/Avi-Rana-1718");
+}
+function LinkedIn() {
+  window.location.replace("https://www.linkedin.com/in/avi-rana/");
+}
+function Twitter() {
+  window.location.replace("https://twitter.com/home");
 }
 
 export default App;
