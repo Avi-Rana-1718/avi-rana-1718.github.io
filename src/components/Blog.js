7d6a69ab70c2dcbc602b4355fc7529e90eb94f9f
Avi Rana <itzavi1718@gmail.com>
2023-12-19 13:00:17 +0530

    working implementation of blogger

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
+import {useParams} from "react-router-dom";
 
-function Blog() {
+function Blog(props) {
 
     const [title, setTitle] = useState(null);
+    const [date, setDate] = useState(null);
+    const [content, setContent] = useState(null);
+
+    const {id} = useParams();
 
 useEffect(
     ()=>{
-        fetch("blogs.json").then(res=>res.json()).then(data=>{
-            setTitle(data.posts[1].title);
+        fetch("/blogs/" + id + ".json").then(res=>res.json()).then(data=>{
+            setTitle(data.title);
+            setContent(data.content);
+            setDate(Date(data.timestamp));
+
         })
     }
 )
 
     return (
         <>
+        <article>
         <h4>{title}</h4>
+        <time>{date}</time>
+        <p>{content}</p>
+        </article>
         </>
     )
 }
