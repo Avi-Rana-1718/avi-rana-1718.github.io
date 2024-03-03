0bc453d6a43b03f8877f81ebe19b8f5c1fd554aa
Avi Rana <itzavi1718@gmail.com>
2024-03-03 14:14:39 +0530

    updates

--- src/app/blogs/[id]/page.js
+++ src/app/blogs/[id]/page.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 
-
 export default async function Blogs({params}) {
 
     let res = await fetch("https://raw.githubusercontent.com/Avi-Rana-1718/avi-rana-1718.github.io/main/public/blogs/" + params.id + ".json", {cache: "no-cache"});
@@ -13,8 +12,8 @@ export default async function Blogs({params}) {
             <article className="bg-[#D9D9D9] p-3 text-left md:w-[50vw] rounded-sm">
                 <span className="text-[#5151c4]"><Link href="/">/</Link></span>
                 <h2 className="text-lg">{data.title}</h2>
-                <time className="text-sm">{date.getDate() + "/" + Number(date.getMonth()+1)+ "/" + date.getFullYear()}</time>
-                <span className="my-2 block" dangerouslySetInnerHTML={{__html: data.content}}></span>
+                <time className="text-sm text-[#757582]">{date.getDate() + "/" + Number(date.getMonth()+1)+ "/" + date.getFullYear()}</time>
+                <span className={`my-2 block `} dangerouslySetInnerHTML={{__html: data.content}}></span>
             </article>
         </section>
     )
