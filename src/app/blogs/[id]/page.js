72ac246143fd9ad086209438a794bc9ac4d29c25
Avi Rana <itzavi1718@gmail.com>
2024-05-05 20:19:41 +0530

    nextjs

---
+++ src/app/blogs/[id]/page.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+
+export default async function Blogs({params}) {
+
+    let res = await fetch("https://raw.githubusercontent.com/Avi-Rana-1718/avi-rana-1718.github.io/main/public/blogs/" + params.id + ".json", {cache: "no-cache"});
+    let data = await res.json();
+
+    let date = new Date(data.timestamp);
+    return (
+        <section className="md:flex justify-center w-full my-16">
+            <article className="bg-[#D9D9D9] p-3 text-left md:w-[50vw] rounded-sm">
+                <span className="text-[#5151c4]"><Link href="/">/</Link></span>
+                <h2 className="text-lg">{data.title}</h2>
+                <time className="text-sm text-[#757582]">{date.getDate() + "/" + Number(date.getMonth()+1)+ "/" + date.getFullYear()}</time>
+                <span className={`my-2 block `} dangerouslySetInnerHTML={{__html: data.content}}></span>
+            </article>
+        </section>
+    )
+}
