b0afd32ed3eff6a69ccb866ab0969559dca2b32d
Avi Rana <itzavi1718@gmail.com>
2024-06-15 03:17:19 +0530

    redesign and rewrite

--- src/components/Navbar.js
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-    return (
-        <nav>
-        <h1 className="text-4xl font-bold">Hi, my name is <span className="bg-[#3A53E3] text-[#fff] p-0.5 rounded-sm">Avi</span>!</h1>
-          </nav>
-    )
-}
