9509121449f6453159e5e57805e8268f7a693001
Avi Rana <itzavi1718@gmail.com>
2024-11-11 18:53:54 +0530

    Changes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -11,14 +11,14 @@ export default function Index() {
                 </p>
              <h5 className="text-3xl font-bold mt-10">Projects</h5>
              <ul>
-                <ListItem label="java-wordle" href="https://github.com/Avi-Rana-1718/java-wordle" />
+                <ListItem label="java-wordle" href="https://github.com/Avi-Rana-1718/java-wordle" className="" />
                 <ListItem label="dishalearning" href="https://github.com/Avi-Rana-1718/dishalearning" />
                 <ListItem label="draw-it" href="https://github.com/Avi-Rana-1718/draw-it" />
                 <ListItem label="Stranded" href="https://github.com/Avi-Rana-1718/Stranded" />
                 <ListItem label="bad-flappybird" href="https://github.com/Avi-Rana-1718/bad-flappybird" />
              </ul>
 
-             <h5 className="text-3xl font-bold mt-10">Contact me</h5>
+             <h5 className="text-3xl font-bold mt-10 ">Contact me</h5>
              <ul>
                 <ListItem label="github" href="https://github.com/Avi-Rana-1718" />
                 <ListItem label="linkedin" href="https://www.linkedin.com/in/avi-rana/" />
