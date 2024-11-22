19186523a34658c755e98ba84435b8b527eecd89
Avi Rana <itzavi1718@gmail.com>
2024-11-22 09:41:25 +0530

    ui changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
     let ageInYears = date.getFullYear()-2004;
 
     return (
-        <nav className="leading-3">
+        <nav className="leading-4">
             <Link to={"/"}>
              <h3 className="text-5xl font-bold">AviRana</h3>
              </Link>
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -31,9 +31,6 @@ export default function Blog() {
             <span className="text-sm text-[#919090] block">{data.byline}</span>
             <time className="text-xs text-[#888]">{new Date(data.timestamp).getDate()+"th "+ (new Date(2024, new Date(data.timestamp).getMonth(), 1).toLocaleString('default', { month: 'long' })) +" " + new Date(data.timestamp).getFullYear()}</time>
             </div>
-            <div className="">
-                <ShareBtn />
-            </div>
         </header>
         <hr className="mt-3 border-t-[#5c5b5b]"/>
         <p className="mt-5" dangerouslySetInnerHTML={{__html: data.content}}></p>
